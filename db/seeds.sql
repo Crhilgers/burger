3b4634ada1b4501394f6387798844e9b5b2f2572
crhilgers <cole5882@gmail.com>
2019-03-07 19:38:32 -0700

    Need to work on commiting before the last minute

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO Burgers (id, name, devoured, date)
+VALUES
+	(1, 'Sadness Hamburger', 1, '2019-03-06 10:51:12'),
+	(2, 'Cheese Burger', 1, '2019-03-06 10:52:15'),
+	(3, 'Complete Bacon Burger', 0, '2019-03-06 10:53:12'),
+	(4, 'Veggie Burger', 0, '2019-03-06 10:54:22'),
+	(5, 'Chicken and Cheese Burger', 0, '2019-03-06 10:55:24');
